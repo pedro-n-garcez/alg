7dc5d03ad11011cacd5a9af7aa2640871a948f7f
pedro-n-garcez <60759070+pedro-n-garcez@users.noreply.github.com>
2020-10-07 04:45:28 -0300

    added fisher-yates algorithm

---
+++ fisher_yates/fisher_yates/FisherYatesShuffle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fisher_yates
+{
+    public static class FisherYatesShuffle
+    {
+        //pseudo-random number generator
+        private static Random rand = new Random();
+
+        //this function takes in a string array and returns the same array but in random order
+        public static string[] shuffledArray(string[] arr)
+        {
+            //going through all items of array from last to first
+            //since array is zero-based, we start at length minus 1, and go down to one
+            for (int i = arr.Length-1; i >= 1; i--)
+            {
+                //j will be a random index inside the array
+                //we'll go down the array, swapping every item until 1 with another item of random j index
+                int j = GetRandomNumberInInterval(i);
+                swapTwoEntries(arr, i, j);
+            }
+            //return the rearranged array
+            return arr;
+        }
+
+        //function that returns a random number from 0 to n
+        //the upper bound for rand.Next is n+1 because the length in the shuffle is minus 1
+        public static int GetRandomNumberInInterval(int n)
+        {
+            return rand.Next(n + 1);
+        }
+
+        //Void function to swap two entries in an array arr
+        //temp is assigned the value of arr[a] so that arr[b] can receive it later
+        //temp serves as the "intermediary" between arr[a] and arr[b]
+        public static void swapTwoEntries(string[] arr, int a, int b)
+        {
+            string temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+    }
+}
---
+++ fisher_yates/fisher_yates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static fisher_yates.FisherYatesShuffle;
+
+//Fisher Yates Shuffle Implementation
+//An algorithm to rearrange a collection of items
+//in random order
+
+namespace fisher_yates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using System.IO function ReadAllLines
+            //to make every line in the file into an entry in an array
+            //the file contains the names of all 50 states
+            string[] entries = File.ReadAllLines("states.txt").ToArray();
+
+            //function from FisherYatesShuffle class shuffles the array of states
+            shuffledArray(entries);
+
+            //write every state, listed in the new order, followed by a comma
+            foreach (string i in entries)
+            {
+                Console.Write($"{i}, ");
+            }
+            //a conclusion to the written list
+            Console.Write("and that's all fifty states.");
+
+            Console.ReadKey();
+        }
+    }
+}
