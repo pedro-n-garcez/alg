aebda59c89f145fe140e471f51a17a328620fdac
pedro-n-garcez <60759070+pedro-n-garcez@users.noreply.github.com>
2020-11-28 02:32:22 -0300

    add project files

---
+++ Sorting/Sorting/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace Sorting
+{
+    class Program
+    {
+        //read text file of scores into an int array
+        static int[] WriteScoresArray()
+        {
+            string[] textInput = File.ReadAllLines("scores.txt");
+            int[] scores = new int[textInput.Length];
+            for (int i = 0; i < textInput.Length; i++)
+            {
+                scores[i] = Int32.Parse(textInput[i]);
+            }
+            return scores;
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            float startTime, endTime, deltaTime;
+
+            //BUBBLESORT
+            int[] scores = WriteScoresArray();
+            Console.WriteLine("----Bubble Sort----");
+            Console.WriteLine("----Best case: O(n), worst case: O(n^2)----");
+            Console.WriteLine("Compares a number with the next one to see if the adjacents are out of order. If current is greater than the next, swapif that is no longer the case, it is sorted.");
+            Console.Write("\nUNSORTED: ");
+            foreach (int i in scores) { Console.Write($"{i} "); }
+            startTime = sw.ElapsedMilliseconds;
+            Sorts.BubbleSort(scores);
+            endTime = sw.ElapsedMilliseconds;
+            deltaTime = endTime - startTime;
+            Console.WriteLine("\nSORTED: ");
+            foreach (int i in scores) { Console.Write($"{i} "); }
+            Console.WriteLine("\nBubble took " + string.Format("{0:0.##}",deltaTime) + "ms" + "\n");
+            float msBubble = deltaTime;
+
+            //INSERTIONSORT
+            scores = WriteScoresArray();
+            Console.WriteLine("----Insertion Sort----");
+            Console.WriteLine("----Best case: O(n), worst case: O(n^2)----");
+            Console.WriteLine("Compares every number with all before it. If the previous is greater than the current one, we swap. This way we'll bring every number to their correct, sorted location.");
+            Console.Write("\nUNSORTED: ");
+            foreach (int i in scores) { Console.Write($"{i} "); }
+            startTime = sw.ElapsedMilliseconds;
+            Sorts.InsertionSort(scores);
+            endTime = sw.ElapsedMilliseconds;
+            deltaTime = endTime - startTime;
+            Console.WriteLine("\nSORTED: ");
+            foreach (int i in scores) { Console.Write($"{i} "); }
+            Console.WriteLine("\nInsertion took " + string.Format("{0:0.##}", deltaTime) + "ms" + "\n");
+            float msInsertion = deltaTime;
+
+            //SELECTIONSORT
+            scores = WriteScoresArray();
+            Console.WriteLine("----Selection Sort----");
+            Console.WriteLine("----Best case: O(n^2), worst case: O(n^2)----");
+            Console.WriteLine("Iterates through the array, looking for the smallest number. Every time it finds a number smaller than the current, that number becomes the smallest. At the end we swap the smallest with the current");
+            Console.Write("\nUNSORTED: ");
+            foreach (int i in scores) { Console.Write($"{i} "); }
+            startTime = sw.ElapsedMilliseconds;
+            Sorts.SelectionSort(scores);
+            endTime = sw.ElapsedMilliseconds;
+            deltaTime = endTime - startTime;
+            Console.WriteLine("\nSORTED: ");
+            foreach (int i in scores) { Console.Write($"{i} "); }
+            Console.WriteLine("\nSelection took " + string.Format("{0:0.##}", deltaTime) + "ms" + "\n");
+            float msSelection = deltaTime;
+
+            //HEAPSORT
+            scores = WriteScoresArray();
+            Console.WriteLine("----Heap Sort----");
+            Console.WriteLine("----Best case: O(n log n), worst case: O(n log n)----");
+            Console.WriteLine("Turns input into a heap data structure, then removes the top item and reorders heap to satisfy its property (parent's two children must be less than or equal to parent) Thus working in a reverse way, the array will be in sorted order.");
+            Console.Write("\nUNSORTED: ");
+            foreach (int i in scores) { Console.Write($"{i} "); }
+            startTime = sw.ElapsedMilliseconds;
+            Sorts.HeapSort(scores);
+            endTime = sw.ElapsedMilliseconds;
+            deltaTime = endTime - startTime;
+            Console.WriteLine("\nSORTED: ");
+            foreach (int i in scores) { Console.Write($"{i} "); }
+            Console.WriteLine("\nHeap took " + string.Format("{0:0.##}", deltaTime) + "ms" + "\n");
+            float msHeap = deltaTime;
+
+            //QUICKSORT
+            scores = WriteScoresArray();
+            Console.WriteLine("----Quick Sort----");
+            Console.WriteLine("----Best case: O(n), worst case: O(n log n)----");
+            Console.WriteLine("Picks a pivot item that divides array into left and right. Items smaller than the pivot go left, items greater go right. Recursive calls subdivide the array further, sorting the smaller divisions until it's all sorted. This is a three-way partition implementation that accounts for repeated elements, so only the less-than and greater-than partitions need to be recursively sorted.");
+            Console.Write("\nUNSORTED: ");
+            foreach (int i in scores) { Console.Write($"{i} "); }
+            startTime = sw.ElapsedMilliseconds;
+            Sorts.QuickSort(scores, 0, scores.Length - 1);
+            endTime = sw.ElapsedMilliseconds;
+            deltaTime = endTime - startTime;
+            Console.WriteLine("\nSORTED: ");
+            foreach (int i in scores) { Console.Write($"{i} "); }
+            Console.WriteLine("\nQuick took " + string.Format("{0:0.##}", deltaTime) + "ms" + "\n");
+            float msQuick = deltaTime;
+
+            //MERGESORT
+            scores = WriteScoresArray();
+            Console.WriteLine("----Merge Sort----");
+            Console.WriteLine("----Best case: O(n log n), worst case: O(n log n)----");
+            Console.WriteLine("Splits array in two, then calls itself to do so again until it can't be divided further. These subdivisions are then merged into sorted groups of two, then four, and so on. The division is always at the middle.");
+            int[] scratch = new int[scores.Length];
+            Console.Write("\nUNSORTED: ");
+            foreach (int i in scores) { Console.Write($"{i} "); }
+            startTime = sw.ElapsedMilliseconds;
+            Sorts.MergeSort(scores, scratch, 0, scores.Length - 1);
+            endTime = sw.ElapsedMilliseconds;
+            deltaTime = endTime - startTime;
+            Console.Write("\nSORTED: ");
+            foreach (int i in scores) { Console.Write($"{i} "); }
+            Console.WriteLine("\nMerge took " + string.Format("{0:0.##}", deltaTime) + "ms" + "\n");
+            float msMerge = deltaTime;
+
+            Console.WriteLine("ALGORITHM\tSTRATEGY\tAV. TIME COMPLEXITY\t\tMILLISECONDS TAKEN\tHOW GOOD IS IT");
+            Console.WriteLine($"BubbleSort\tComparison\tO(n^2)\t\t\t\t{msBubble}\t\t\tGood for smaller lists, too slow for large lists");
+            Console.WriteLine($"InsertionSort\tInsertion\tO(n^2)\t\t\t\t{msInsertion}\t\t\tGood for smaller lists, generally best-performant of O(n^2) sorts");
+            Console.WriteLine($"SelectionSort\tComparison\tO(n^2)\t\t\t\t{msSelection}\t\t\tGood for smaller lists, slow for larger lists");
+            Console.WriteLine($"HeapSort\tComparison\tO(n log n)\t\t\t{msHeap}\t\t\tSimilar to Selection but faster on large lists");
+            Console.WriteLine($"QuickSort\tDivide&conquer\tO(n log n)\t\t\t{msQuick}\t\t\tUsually the fastest of n log n sorts");
+            Console.WriteLine($"MergeSort\tDivide&conquer\tO(n log n)\t\t\t{msMerge}\t\t\tGood for large lists");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sorting/Sorting/Sorts.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public static class Sorts
+    {
+        public static void BubbleSort(int[] arr)
+        {
+            bool notSorted = true;
+            while (notSorted)
+            {
+                notSorted = false;
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    //compare element with its immediate successor
+                    if (arr[i] > arr[i + 1])
+                    {
+                        //swap if not in correct order
+                        Swap(arr, i, i + 1);
+                        notSorted = true;
+                    }
+                }
+                //if notSorted remains false, no more swaps are needed
+            }
+        }
+
+        public static void InsertionSort(int[] arr)
+        {
+            //we start with 1 to compare arr[1] w arr[0], arr[2] w arr[1] & so on
+            for (int i = 1; i<arr.Length; i++)
+            {
+                //index of current number we're working with
+                int j = i;
+                //checking if j>0 to end while when we reach the end
+                //if the previous num is bigger than current, swap
+                while (j > 0 && arr[j - 1] > arr[j])
+                {
+                    Swap(arr, j - 1, j);
+                    j--;
+                }
+            }
+        }
+
+        public static void SelectionSort(int[] arr)
+        {
+            int min;
+            //i is the current number, which we'll swap with the smallest at the end
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                //i assumed to be the smallest until we find a smaller one, and so on
+                min = i;
+                //nested for loop to compared subsequent elements with min
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    //if true, then we've found a smaller number, pass than as the new minimum 
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+                //we only get to swap until after the nested for is done iterating
+                //which means we've found the smallest number out of all the elements
+                Swap(arr, i, min);
+            }
+        }
+
+        //Makes use of MakeHeap and Remove helper functions
+        public static void HeapSort(int[] arr)
+        {
+            //heapify the array
+            MakeHeap(arr);
+
+            //iterate thru heap, from the bottom up to the top node
+            for (int i = arr.Length-1; i >= 0; i--)
+            {
+                //swap top and bottom nodes
+                Swap(arr,0,i);
+
+                //restore the heap property
+                //pass i in the function because arr[i] is considered removed from heap
+                //so the size of the heap changes according to i in for
+                RemoveTopItemFromHeap(arr, i);
+            }
+        }
+
+        //Uses partition helper function, a tuple that returns leftmost and rightmost items of partition
+        public static void QuickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                //assign tuple value from partition to p variable
+                Tuple<int, int> p = Partition(arr, low, high);
+
+                //assign left and right indices of partition
+                int left = p.Item1;
+                int right = p.Item2;
+
+                //recursive calls to subdivide further and sort the smaller parts
+                QuickSort(arr, low, left - 1);
+                QuickSort(arr, right, high);
+            }
+        }
+
+        //Uses empty scratch of same length as arr, where sorting occurs. Values are copied back to arr
+        public static void MergeSort(int[] arr, int[] scratch, int start, int end)
+        {
+            //end function if only one element in arr, it's already sorted then
+            if (start == end)
+            {
+                return;
+            }
+
+            //find the middle index of the array
+            int midpoint = (start + end) / 2;
+
+            //recursive call to divide array in half, and that half in half, and so on
+            MergeSort(arr, scratch, start, midpoint);
+            MergeSort(arr, scratch, midpoint+1, end);
+
+            //find where the left, right and scratch iterations begin
+            int leftIndex = start;
+            int rightIndex = midpoint + 1;
+            int scratchIndex = leftIndex;
+            while(leftIndex <= midpoint && rightIndex <= end)
+            {
+                if (arr[leftIndex] <= arr[rightIndex])
+                {
+                    scratch[scratchIndex] = arr[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    scratch[scratchIndex] = arr[rightIndex];
+                    rightIndex++;
+                }
+                scratchIndex++;
+            }
+
+            //Copy into scratch the remaining elements
+            for (int i = leftIndex; i <= midpoint; i++)
+            {
+                scratch[scratchIndex] = arr[i];
+                scratchIndex++;
+            }
+
+            //Copy into scratch the remaining elements
+            for (int i = rightIndex; i <= end; i++)
+            {
+                scratch[scratchIndex] = arr[i];
+                scratchIndex++;
+            }
+
+            //this copies the contents of scratch back into arr
+            for (int i = start; i <= end; i++)
+            {
+                arr[i] = scratch[i];
+            }
+        }
+
+        //Swap helper function used in multiple sorting algorithms
+        //arguments: array & index positions of elements we wanna swap
+        private static void Swap(int[] arr, int a, int b)
+        {
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+
+        //Partition helper function for QuickSort
+        private static Tuple<int, int> Partition(int[] arr, int low, int high)
+        {
+            //iterate from the first item
+            int i = low;
+            //pivot at the last item
+            int pivot = arr[high];
+            while (i <= high)
+            {
+                //everything less than pivot goes left
+                if (arr[i] < pivot)
+                {
+                    Swap(arr, low, i);
+                    low++;
+                    i++;
+                }
+                else if (arr[i] > pivot) //everything greater than pivot goes right
+                {
+                    Swap(arr, high, i);
+                    high--;
+                } //items equal to pivot don't need sorting, continue iterating
+                else { i++; }
+            } //return leftmost and rightmost item of partition
+            return Tuple.Create(low, i);
+        }
+
+        //MakeHeap helper function for HeapSort
+        /*turns array into a heap structure
+         * parent followed by its two children, followed by their children, and so on
+         * every child is less than or equal to its parent
+         * if there aren't enough nodes, some children will be childless
+         */
+        private static void MakeHeap(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //we start with index as the child, finding its parents until we reach the root
+                int index = i;
+                while (index != 0)
+                {
+                    //finds the index of the parent
+                    int parent = (index - 1) / 2;
+                    //if heap property is satisfied, break
+                    if (arr[index] <= arr[parent])
+                    {
+                        break;
+                    } //swap if child is greater than parent
+                    else {
+                        Swap(arr, index, parent);
+                    } //we then go to the parent's index and keep working our way up
+                    index = parent;
+                }
+            }
+        }
+
+        //Helper function to remove and swap the top item in a heap for HeapSort
+        private static void RemoveTopItemFromHeap(int[] arr, int i)
+        {
+            int index = 0;
+            while (true)
+            {
+                //finds the indices of the two children
+                int child1 = 2 * index + 1;
+                int child2 = 2 * index + 2;
+
+                //check if child index is out the bounds of the heap
+                if (child1 >= i) { child1 = index; }
+                if (child2 >= i) { child2 = index; }
+
+                //break if heap property is satisfied (both children less than or equal to parent
+                if (arr[index] >= arr[child1] && arr[index] >= arr[child2]) { break; }
+
+                //whichever child is greater will be swapped with the parent
+                int swapChild = (arr[child1] > arr[child2] ? child1 : child2);
+
+                //swap parent and child if child greater than parent
+                if (arr[swapChild] > arr[index])
+                {
+                    Swap(arr, index, swapChild);
+                }
+                //pick up from the child; child becomes the next parent
+                index = swapChild;
+            }
+        }
+    }
+}
