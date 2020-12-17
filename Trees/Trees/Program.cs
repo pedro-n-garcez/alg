e40102e7034a8e157f4767a2f54251a489a6662e
pedro-n-garcez <60759070+pedro-n-garcez@users.noreply.github.com>
2020-12-16 22:09:12 -0300

    add project files

---
+++ Trees/Trees/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Trees
+{
+    class Program
+    {
+        //read text file of scores into an int array
+        static int[] WriteScoresArray()
+        {
+            string[] textInput = File.ReadAllLines("scores.txt");
+            int[] scores = new int[textInput.Length];
+            for (int i = 0; i < textInput.Length; i++)
+            {
+                scores[i] = Int32.Parse(textInput[i]);
+            }
+            return scores;
+        }
+
+        static void Main(string[] args)
+        {
+            //write array
+            int[] scores = WriteScoresArray();
+            //since this is a project about trees, might as well use HeapSort
+            Heaps.HeapSort(scores);
+
+            Console.WriteLine("------SORTED ARRAY:------");
+            foreach (int i in scores)
+            {
+                Console.Write($"{i} ");
+            } Console.WriteLine("\n");
+
+            BinaryTree tree = new BinaryTree();
+            BinaryNode root = null;
+
+            foreach (int i in scores)
+            {
+                root = tree.Insert(root, i);
+            }
+
+            Console.WriteLine("------INORDER TRAVERSAL & PRINT EACH NODE MULTIPLIED BY TWO:------");
+            tree.Traverse(root);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Trees/Trees/Tree.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    //We can create a node as a class
+    //Otherwise we could use a struct for example
+    public class BinaryNode
+    {
+        //the node contains references to its left and right children
+        //in addition to the data it holds
+        public int Data;
+        public BinaryNode Left;
+        public BinaryNode Right;
+
+        //constructor to pass in the data along with the node instantiation
+        public BinaryNode(int Data)
+        {
+            this.Data = Data;
+        }
+    }
+
+    //A class for the tree itself
+    public class BinaryTree
+    {
+        //Function to insert a new node at the root
+        public BinaryNode Insert(BinaryNode root, int data)
+        {
+            //if root is empty, insert a new node
+            if (root == null)
+            {
+                root = new BinaryNode(data);
+            }
+            //integers less than the root's stay on the left
+            //recursive call to insert to the left
+            else if (data < root.Data)
+            {
+                root.Left = Insert(root.Left, data);
+            }
+            //integers greater than or equal to the root's stay on the right
+            //recursive call to insert to the right
+            else
+            {
+                root.Right = Insert(root.Right, data);
+            }
+            return root;
+        }
+
+        //Inorder traversal
+        /*
+         * First, we traverse the left subtree by recursive call
+         * Then, we access the data
+         * Finally, we traverse the right subtree by recursive call
+         */
+         //As example of what we can do with traversal, an operation is performed on each node data
+        public void Traverse(BinaryNode root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            Traverse(root.Left);
+            Console.Write($"{root.Data * 2} ");
+            Traverse(root.Right);
+        }
+    }
+}
